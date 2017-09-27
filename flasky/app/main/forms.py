5d7037a0331df07f1bd2f224b17f49aafff9c1cd
jxw1 <2575173743@qq.com>
2017-09-27 18:31:59 +0800

    first commit

---
+++ flasky/app/main/forms.py
@@ -0,0 +1,94 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, SelectField,\
+    TextAreaField, IntegerField
+from wtforms.validators import Required, Length, Email, Regexp
+from ..models import Role, User, Book, Record
+from wtforms import ValidationError
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    realname = StringField('Real name', validators=[Length(0, 64)])
+    submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    realname = StringField('real name', validators=[Length(0, 64)])
+    submit = SubmitField('submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                    User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError("Username already in use")
+
+
+class AddBookForm(FlaskForm):
+    bookname = StringField('book name', validators=[Required()])
+    info = TextAreaField('book info', validators=[Required()])
+    author = StringField('author name', validators=[Required()])
+    totalnumber = IntegerField('the totalnumber of book', validators=[Required()])
+    subject_id = IntegerField('the subject_id of book')
+    ISBN = StringField('ISBN', validators=[Required()])
+    publisher = StringField('publisher', validators=[Required()])
+    location = StringField('the location of the book', validators=[Required()])
+    submit = SubmitField('submit')
+
+    def validate_ISBN(self, field):
+        if Book.query.filter_by(ISBN=field.data).first():
+            raise ValidationError('the book already existed.')
+
+
+class EditBookForm(FlaskForm):
+    bookname = StringField('book name', validators=[Required()])
+    info = TextAreaField('book info', validators=[Required()])
+    author = StringField('author name', validators=[Required()])
+    subject_id = IntegerField('the subject_id of book')
+    ISBN = StringField('ISBN', validators=[Required()])
+    publisher = StringField('publisher', validators=[Required()])
+    location = StringField('the location of the book', validators=[Required()])
+    submit = SubmitField('submit')
+
+
+class SearchBookForm(FlaskForm):
+    # choices = [('bookname')]
+    # type = SelectField
+    input_ = StringField('book name', validators=[Required()])
+    submit = SubmitField('search book')
+
+
+class BorrowForm(FlaskForm):
+    sequence = IntegerField('the sequence number of the book', validators=[Required()])
+    user_id = IntegerField('the id of the borrower', validators=[Required()])
+    submit = SubmitField('submit')
+
+
+class ReturnForm(FlaskForm):
+    sequence = IntegerField('the sequence number of the book', validators=[Required()])
+    submit = SubmitField('submit')
+
+
+class SearchUserForm(FlaskForm):
+    username = StringField('the username of the user', validators=[Required()])
+    submit = SubmitField('submit')
---
+++ library2/flasky/app/main/views.py
@@ -0,0 +1,228 @@
+from flask import render_template, redirect, url_for, session, abort, flash
+from . import main
+from ..import db
+from ..models import Role, User, Book, Book_entity, Record
+from .forms import EditProfileForm, EditProfileAdminForm,\
+    AddBookForm, SearchBookForm, EditBookForm, BorrowForm,\
+    ReturnForm, SearchUserForm
+from flask_login import login_required, current_user
+from ..decorators import admin_required, lib_required
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index(books=[]):
+    form = SearchBookForm()
+    if form.validate_on_submit():
+        bookname = form.input_.data
+        session['input_data'] = bookname
+        books = Book.query.filter(Book.bookname.ilike('%'+bookname+'%')).all()
+        return render_template('index.html', form=form, books=books)
+    return render_template('index.html', form=form, books=books)
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    # 此处可以增加用户借书记录查看
+    records = user.Records
+    books = []
+    for record in records:
+        book = Book.query.filter_by(id=record.book_id).first_or_404()
+        books.append(book)
+    return render_template('user.html', user=user, books=books)
+
+@main.route('/book/<book_id>', methods=['GET', 'POST'])
+def book(book_id):
+    book = Book.query.filter_by(id=book_id).first_or_404()
+    book_entities = book.book_entities.all()
+    nowNumber = 0
+    for book_entity in book_entities:
+        if(book_entity.confirmed):
+            nowNumber += 1
+    return render_template('book_info.html', book=book, nowNumber=nowNumber)
+
+
+@main.route('/del_book/<book_id>',  methods=['GET', 'POST'])
+@login_required
+@lib_required
+def del_book(book_id):
+    book = Book.query.filter_by(id=book_id).first_or_404()
+    bookname = book.bookname
+    db.session.delete(book)
+    flash(" the book: " + bookname + " has been deleted")
+    return redirect(url_for('main.index'))
+
+
+@main.route('/edit_book/<book_id>', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def edit_book(book_id):
+    book = Book.query.filter_by(id=book_id).first_or_404()
+    form = EditBookForm()
+    oldname = book.bookname
+    if form.validate_on_submit():
+        book.bookname = form.bookname.data
+        book.info = form.info.data
+        book.author = form.author.data
+        book.publisher = form.publisher.data
+        book.subject_id = form.subject_id.data
+        book.location = form.location.data
+        book.ISBN = form.ISBN.data
+        db.session.add(book)
+        flash("the book: <"+oldname+"> has been modified ")
+        return redirect(url_for('main.book', book_id=book.id))
+    form.bookname.data = book.bookname
+    form.info.data = book.info
+    form.author.data = book.author
+    form.publisher.data = book.publisher
+    form.subject_id.data = book.subject_id
+    form.location.data = book.location
+    form.ISBN.data = book.ISBN
+    return render_template('edit_book.html', form=form, book=book)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.realname = form.realname.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.realname.data = current_user.realname
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.realname = form.realname.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.realname.data = user.realname
+    return render_template('edit_profile.html', form=form, user=user)
+
+
+@main.route('/add_book', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def add_book():
+    form = AddBookForm()
+    if form.validate_on_submit():
+        book = Book(bookname=form.bookname.data,
+                    totalNumber=form.totalnumber.data,
+                    subject_id=form.subject_id.data,
+                    author=form.author.data,
+                    ISBN=form.ISBN.data,
+                    info=form.info.data,
+                    publisher=form.publisher.data,
+                    location=form.location.data)
+        db.session.add(book)
+        db.session.commit()
+        for i in range(book.totalNumber):
+            book_entity = Book_entity(book_id=book.id)
+            db.session.add(book_entity)
+        flash("the book has been added")
+        return redirect(url_for('main.add_book'))
+    return render_template('add_book.html', form=form)
+
+
+@main.route('/borrow_book', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def borrow_book():
+    form = BorrowForm()
+    if form.validate_on_submit():
+        sequenceNumber = form.sequence.data
+        bookCopy = Book_entity.query.get_or_404(sequenceNumber)
+        book_id = bookCopy.book_id
+        user_id = form.user_id.data
+        if(bookCopy.can_borrow()):
+            user = User.query.get_or_404(user_id)
+            if(user.can_borrow()):
+                bookCopy.confirmed = False
+                user.borrowed_number += 1
+                bookCopy.create_record(book_id, user_id)
+                db.session.add(user)
+                db.session.add(bookCopy)
+                flash("borrow successfully")
+                return redirect(url_for('main.borrow_book'))
+            else:
+                flash('''the person can't borrow book''')
+                return redirect(url_for('main.borrow_book'))
+        else:
+            flash("the book hasn't been returned")
+            return redirect(url_for('main.borrow_book'))
+    return render_template('borrow_book.html', form=form)
+
+
+@main.route('/return_book', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def return_book():
+    form = ReturnForm()
+    if form.validate_on_submit():
+        sequenceNumber = form.sequence.data
+        bookReturned = Book_entity.query.get_or_404(sequenceNumber)
+        if(bookReturned.can_return()):
+            if(bookReturned.book_return()):
+                flash("return book successful")
+                return redirect(url_for('main.return_book'))
+            else:
+                flash("return book failed")
+                return redirect(url_for('main.return_book'))
+        else:
+            flash("the book is already returned")
+            return redirect(url_for('main.return_book'))
+    else:
+        return render_template('return_book.html', form=form)
+
+
+@main.route('/search_user', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def search_user(users=[]):
+    form = SearchUserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        users = User.query.filter(User.username.ilike('%'+username+'%')).all()
+        return render_template('search_user.html', form=form, users=users)
+    return render_template('search_user.html', form=form, users=users)
+
+
+@main.route('/check_records', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def check_records():
+    records = Record.query.order_by(Record.returned.desc()).all()
+    return render_template('check_records.html', records=records)
+
+
+@main.route('/del_user/<user_id>', methods=['GET', 'POST'])
+@login_required
+@lib_required
+def del_user(user_id):
+    user = User.query.filter_by(id=user_id).first_or_404()
+    if(user.borrowed_number is 0):
+        records = user.Records
+        for record in records:
+            db.session.delete(record)
+        flash("delete the user " + user.username + " successful")
+        db.session.delete(user)
+    else:
+        flash("delete the user unsuccessfully")
+    return redirect(url_for('main.search_user'))
